7759c0ba047a49b0d5f807dcf5601c5874b81e6f
Sanskruti Gunde <sanskruti.gunde@gmail.com>
2024-01-14 23:28:54 +0530

    WhileLoop Exercise

---
+++ WhileLoop.py
@@ -0,0 +1,13 @@
+# x = 0
+# while x <5:
+#     print(x)
+#     x = x + 1
+    
+#------------------------------------------
+
+x = int(input("x: "))
+if x > 5:
+    print("number is greater than 5")
+while x <=5:
+    print(x)
+    x = x + 1
