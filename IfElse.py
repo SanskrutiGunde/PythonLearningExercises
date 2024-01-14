fdf19195bdfee439b024f49e4694ecc1eba144d3
Sanskruti Gunde <sanskruti.gunde@gmail.com>
2024-01-14 23:28:41 +0530

    IfElse exercise

---
+++ IfElse.py
@@ -0,0 +1,17 @@
+'''x = 3
+if x < 5:
+ x = 10
+else:
+ x = 2
+ 
+ x = input("x: ")   
+print(x )'''
+
+#----------------------------------
+
+name = "Sanskruti"
+if name == "Gunde":
+    print("name verified")
+else:
+    print("unrecognized name")
+    
